60edeb05e756029baa7aa2aa9928f03a16cc5636
Devsys <devsys>
2026-05-18 14:51:21 -0300

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by DevSys on 18/05/26.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
